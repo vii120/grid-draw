b0ff6705f22ee164d60bd5a57017b355de41da00
Vivi Tseng <vii0726@gmail.com>
2020-10-14 14:24:21 +0800

    ADD: Add deploy.sh

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/grid-draw/' : '/',
   devServer: {
     port: 8077,
   },
